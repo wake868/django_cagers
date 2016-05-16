f1ac3935a9e8eacf413ba4c2be7907fdbb4967b9
Wakeland <wakelandm@CraftsAmericana.com>
2016-05-16 13:56:19 -0700

    tutorial

--- articles/models.py
+++ articles/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
+class Player(models.Model):
     player_name = models.CharField(max_length=200)
     player_number = models.IntegerField
     player_team = models.CharField(max_length=200)
