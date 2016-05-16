7e493e4d6d4f8cbacb07b6e7ed87572d763f79b5
Wakeland <wakelandm@CraftsAmericana.com>
2016-05-16 13:51:50 -0700

    tutorial

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'articles.views.index', name='index'),
+urlpatterns = [
     url(r'^articles/', include('articles.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^admin/', admin.site.urls),
+]
